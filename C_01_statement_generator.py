e167b77d9d499f2e55cc3f89b2ca6a2a97244845
CleverSkull <sutherlandc3085@masseyhigh.school.nz>
2026-02-05 14:35:45 +1300

    Add files via upload

---
+++ C_01_statement_generator.py
@@ -0,0 +1,4 @@
+def statement_generator(statement, decoration):
+    """Emphasises headings by adding decoration at the start and end"""
+    print(f"{decoration * 3} {statement} {decoration * 3}")
+
